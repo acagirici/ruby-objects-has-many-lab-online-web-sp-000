f504900f79a232a39591f52fa830705371950a15
Arif Cagirici <acagirici23@gmail.com>
2020-06-27 03:35:38 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,8 +1,33 @@
 class Author
+
   attr_accessor :name
- 
- def initialize(name)
+
+  @@post_count = 0
+
+  def initialize(name)
     @name = name
+    @posts = []
   end
-  
-end 
+
+  def posts
+    @posts
+  end
+
+  def add_post(post)
+    post.author = self
+    @posts << post
+    @@post_count += 1
+  end
+
+  def add_post_by_title(postname)
+    post = Post.new(postname)
+    post.author = self
+    @posts << post
+    @@post_count += 1
+  end
+
+  def self.post_count
+    @@post_count
+  end
+
+end
