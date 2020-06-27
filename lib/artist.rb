52c9bc024d721503ae7be7f3782fe29f7efacd16
Arif Cagirici <acagirici23@gmail.com>
2020-06-27 03:49:26 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,21 +6,18 @@ class Artist
 
   def initialize(name)
     @name = name
-    @songs = []
   end
 
   
 
   def add_song(song)
     song.artist = self
-    @songs << song
     @@song_count += 1
   end
 
   def add_song_by_name(songname)
     song = Song.new(songname)
     song.artist = self
-    @songs << song
     @@song_count += 1
   end
 
@@ -28,6 +25,7 @@ class Artist
     @@song_count
   end
 def songs
-    @songs 
+    Song.all.select {|song| song.artist == self
+  end
   end
 end
