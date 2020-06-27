2da216f4f4ca563e05164ad2e896f338487c6db4
Arif Cagirici <acagirici23@gmail.com>
2020-06-27 03:39:08 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,13 +2,19 @@ class Post
 
   attr_accessor :author
   attr_reader :title
-
+    
+    @@all = []
   def initialize(title)
     @title = title
+    @@all << self 
   end
 
   def author_name
     self.author.nil? ? nil : self.author.name
   end
-
+  
+  def self.all
+    @@all
+  end
+  
 end
